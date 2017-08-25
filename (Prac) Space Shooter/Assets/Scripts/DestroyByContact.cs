3b115ff08372a4aa0d9e5ec40a8c14bcd99be6a9
alhulljar <31191366+alhulljar@users.noreply.github.com>
2017-08-24 23:13:09 -0400

    Completed Project
    Ready to Build

---
+++ (Prac) Space Shooter/Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour {
+
+    public GameObject explosion;
+    public GameObject playerExplosion;
+    public int value;
+    private GameController gameController;
+
+    void Start()
+    {
+        gameController = GameController.FindObjectOfType<GameController>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Boundary"))
+        {
+            return;
+        }
+        Instantiate(explosion, transform.position, transform.rotation);
+        if (other.CompareTag("Player"))
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+            gameController.GameOver();
+        }
+        gameController.AddScore(value);
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
