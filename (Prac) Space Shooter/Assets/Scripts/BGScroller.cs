a6f8d7a0cd6924a463767ea5eb5dd026ed2bdcd8
alhulljar <31191366+alhulljar@users.noreply.github.com>
2017-08-27 20:53:25 -0400

    Project Extended
    Added Scrolling Background, Enemy Ships, and Multible variations of Asteroid. Also learned some new things. Thanks Unity!

---
+++ (Prac) Space Shooter/Assets/Scripts/BGScroller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScroller : MonoBehaviour {
+
+    public float scrollSpeed;
+    public float tileSizeZ;
+
+    private Vector3 startPos;
+	
+	void Start ()
+    {
+        startPos = transform.position;
+	}
+	
+	void Update ()
+    {
+        float newPosition = Mathf.Repeat(Time.time * scrollSpeed,tileSizeZ);
+        transform.position = startPos + Vector3.forward * newPosition;
+	}
+}
